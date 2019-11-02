e08976e3e4bc2c84416ff53015876d766d847eaf
Vitaly.Tartynov <Vitaly.Tartynov@GMail.com>
2019-11-02 19:38:15 +0200

    Added husky, eslint. Added iso sample.

--- src/index.js
+++ src/index.js
@@ -1,32 +1,64 @@
-import Phaser from "phaser";
-import tile from "../assets/tile.png";
+import Phaser, { Game, Scene } from 'phaser'
+import IsoPlugin from 'phaser3-plugin-isometric/src/IsoPlugin'
+
+import tile from '../assets/tile.png'
+
+class IsoGame extends Scene {
+  constructor () {
+    const sceneConfig = {
+      key: 'IsoInteractionExample',
+      mapAdd: { isoPlugin: 'iso' }
+    }
+
+    super(sceneConfig)
+  }
+
+  preload () {
+    this.load.image('tile', tile)
+
+    this.load.scenePlugin({
+      key: 'IsoPlugin',
+      url: IsoPlugin,
+      sceneKey: 'iso'
+    })
+  }
+
+  create () {
+    this.isoGroup = this.add.group()
+
+    this.iso.projector.origin.setTo(0.5, 0.3)
+
+    this.spawnTiles()
+  }
+
+  spawnTiles () {
+    let t
+    for (let x = 0; x < 512; x += 38) {
+      for (let y = 0; y < 512; y += 38) {
+        t = this.add.isoSprite(x, y, 0, 'tile', this.isoGroup)
+        t.setInteractive()
+
+        t.on('pointerover', function () {
+          this.tint = 0x86bfda
+          this.isoZ += 5
+        })
+
+        t.on('pointerout', function () {
+          this.tint = 0xffffff
+          this.isoZ -= 5
+        })
+      }
+    }
+  }
+}
 
 const config = {
   type: Phaser.AUTO,
-  parent: "phaser-example",
   width: 800,
   height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
-};
-
-const game = new Phaser.Game(config);
-
-function preload() {
-  this.load.image("tile", tile);
+  pixelArt: true,
+  scene: IsoGame
 }
 
-function create() {
-  const logo = this.add.image(400, 150, "tile");
-
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
-}
+// eslint-disable-next-line no-new
+new Game(config)
